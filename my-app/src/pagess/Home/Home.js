0e728363122b625f2818a8a05bc7dafd3980c859
Ali <ali_mohamadi2017@yahoo.com>
2021-11-15 19:27:00 +0330

    start

--- my-app/src/pagess/Home/Home.js
+++ my-app/src/pagess/Home/Home.js
@@ -1,4 +1,4 @@
-import  "./Home.css"
+import "./Home.css"
 import Posts from "../../post/posts"
 import Sidebar from "../../sidebar/sidebar"
 import Header from '../../Header/Header'
@@ -6,11 +6,12 @@ import Header from '../../Header/Header'
 function Home() {
     return (
         <>
-          <Header/>
-        <div className="home">
-            <Posts/>
-            <Sidebar/>
-        </div>
+            <Header />
+            <div className="home">
+                <Posts />
+                <Sidebar />
+
+            </div>
         </>
     )
 }
